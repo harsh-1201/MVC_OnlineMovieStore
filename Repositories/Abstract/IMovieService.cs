af8cbd54e303e032c913e37e6a79913f2c403bc4
harsh-1201 <harshalc070@gmail.com>
2025-03-06 12:11:01 +0530

    Add files via upload

---
+++ Repositories/Abstract/IMovieService.cs
@@ -0,0 +1,23 @@
+﻿using MovieStoreMvc.Models.Domain;
+using MovieStoreMvc.Models.DTO;
+
+namespace MovieStoreMvc.Repositories.Abstract
+{
+    public interface IMovieService
+    {
+       bool Add(Movie model);
+       bool Update(Movie model);
+       Movie GetById(int id);
+       bool Delete(int id);
+       MovieListVm List(string term = "", bool paging = false, int currentPage = 0);
+        List<int> GetGenreByMovieId(int movieId);
+
+    }
+}
+
+//public interface IMovieService
+//{
+//    MovieListVm List(string term, bool isActive, int currentPage, int pageSize);
+//    MovieDto GetById(int movieId);
+//}
+
